c504b42c1d816f864ebd58479daa89789d4a7559
hiep <daihieptn97@gmail.com>
2019-10-19 21:53:49 +0700

    dev 4

--- views/home.php
+++ views/home.php
@@ -1,38 +1,47 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<title>Trang chủ</title>
-	<link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <title>Trang chủ</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
-	<div class="container border p-t-20">
-		<div class="title">
-			<h4>Danh sách sách</h4>
-		</div>
-		<a href="?page=them" >
-			Thêm sách
-		</a>
-		<table  width="100%">
-			<thead>
-				<tr>
-					<th>ID</th>
-					<th>Tên sách</th>
-					<th>
-						<a href="?page=index&sort=<?php if($_GET['sort'] == "desc") echo "asc"; else echo "desc" ?>">Giá sách</a>
-					</th>
-					<th>Nhà xuất bản</th>
-					<th>Thời gian</th>
-					<th></th>
-				</tr>
-            
-			</thead>
-			<tbody>
-            
+<div class="container border p-t-20">
+    <div class="title">
+        <h4>Danh sách sách</h4>
+    </div>
+    <a href="?page=them">
+        Thêm sách
+    </a>
+    <table width="100%">
+        <thead>
+        <tr>
+            <th>ID</th>
+            <th>Tên sách</th>
+            <th>
+                <a href="?page=index&sort=<?php if ($_GET['sort'] == "desc") echo "asc"; else echo "desc" ?>">Giá
+                    sách</a>
 
-			</tbody>
-		</table>
-	</div>
+
+                <a href="?page=index&sort=<?php if ($_GET['sort'] == "desc") echo "asc"; else echo "desc" ?>">Giá
+                    sách</a>
+                <a href="?page=index&sort=<?php if ($_GET['sort'] == "desc") echo "asc"; else echo "desc" ?>">Giá
+                    sách</a>
+
+
+            </th>
+            <th>Nhà xuất bản</th>
+            <th>Thời gian</th>
+            <th></th>
+        </tr>
+
+        </thead>
+        <tbody>
+
+
+        </tbody>
+    </table>
+</div>
 </body>
 </html>
